363ea5fe905a07997a05c36d85bdd092504aa397
BCRevels <31749493+BCRevels@users.noreply.github.com>
2020-03-03 11:54:23 -0600

    Picture Grabbing Script

--- picture-grabber.sh
+++ picture-grabber.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+if [ ! -f "./ctkeeper.txt" ] ; then
+    touch "ctkeeper.txt"
+    echo "1" > "ctkeeper.txt"
+fi
+
+subset=$(more "ctkeeper.txt")
+
+curl --output `echo "./Set$subset/Fresh/FR$(date +"%s").jpg"` "http://freshfoodslivecam.usm.edu/axis-cgi/jpg/image.cgi?resolution=1280x720"
+curl --output `echo "./Set$subset/Starbucks/SB$(date +"%s").jpg"` "http://starbuckslivecam.usm.edu/axis-cgi/jpg/image.cgi?resolution=1280x720"
+
+subset=$(echo "$subset % 4 + 1" | bc)
+echo $subset > "ctkeeper.txt"
