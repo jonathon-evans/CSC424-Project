4795f8f5d285e03162524747ad8726d5044e334f
BCRevels <31749493+BCRevels@users.noreply.github.com>
2020-03-08 14:23:52 -0500

    Update classes and script

--- picture-grabber.sh
+++ picture-grabber.sh
@@ -5,10 +5,22 @@ if [ ! -f "./ctkeeper.txt" ] ; then
     echo "1" > "ctkeeper.txt"
 fi
 
+hour=$(date +"%H")
+dow=$(date +"%u")
 subset=$(more "ctkeeper.txt")
 
-curl --output `echo "./Set$subset/Fresh/FR$(date +"%s").jpg"` "http://freshfoodslivecam.usm.edu/axis-cgi/jpg/image.cgi?resolution=1280x720"
-curl --output `echo "./Set$subset/Starbucks/SB$(date +"%s").jpg"` "http://starbuckslivecam.usm.edu/axis-cgi/jpg/image.cgi?resolution=1280x720"
+if [ $hour -ge 7 ] && [ $hour -le 21 ] ; then
+    if [ $dow -le 4 ] || { [ $dow -ge 5 ] && [ $dow -le 6 ] && [ $hour -le 19 ] ; } || { [ $dow -eq 7 ] && [ $hour -ge 9 ] ; } ; then
+        curl --output `echo "./Set$subset/Fresh/FR$(date +"%s").jpg"` "http://freshfoodslivecam.usm.edu/axis-cgi/jpg/image.cgi?resolution=1280x720"
+    fi
+fi
+
+if [ $hour -ge 7 ] ; then
+    if [ $dow -le 4 ] || { [ $dow -eq 5 ] && [ $hour -le 19 ] ; } || { [ $dow -eq 6 ] && [ $hour -ge 10 ] && [ $hour -le 18 ] ; } || { [ $dow -eq 7 ] && [ $hour -ge 12 ] ; } ; then
+        curl --output `echo "./Set$subset/Starbucks/SB$(date +"%s").jpg"` "http://starbuckslivecam.usm.edu/axis-cgi/jpg/image.cgi?resolution=1280x720"
+    fi
+fi
+
 
 subset=$(echo "$subset % 4 + 1" | bc)
 echo $subset > "ctkeeper.txt"
